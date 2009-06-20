7df7ba922f228ad8e3a5032902d400713402b872
g.himanshu <g.himanshu@3e609594-5421-11de-833c-a7bd19eed978>
2009-06-20 15:47:42 +0000

    changing the strategy for primitive procedure impl, rather than creating one class per primitive procedure, all of'em will go to one class PrimitiveProcedure which has different id for different primitive procedure

--- trunk/src/main/java/jasi/semantics/Environment.java
+++ trunk/src/main/java/jasi/semantics/Environment.java
@@ -1,11 +1,11 @@
 package jasi.semantics;
 
-import jasi.semantics.procedure.PlusPrimitiveProcedure;
+import jasi.semantics.procedure.PrimitiveProcedure;
 
 public class Environment {
 
     public Object getVariableValue(String vName) {
         //right now just returns plus primitive procedure.
-        return new PlusPrimitiveProcedure();
+        return new PrimitiveProcedure(PrimitiveProcedure.PLUS);
     }
 }
