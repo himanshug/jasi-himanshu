03be95eaac759531f89b6e48d22c69cf8775b332
g.himanshu <g.himanshu@3e609594-5421-11de-833c-a7bd19eed978>
2009-06-21 16:06:23 +0000

    putting in the read primitive procedure impl

--- trunk/src/main/java/jasi/Main.java
+++ trunk/src/main/java/jasi/Main.java
@@ -1,7 +1,9 @@
 package jasi;
 
+import jasi.datatype.SVariable;
 import jasi.parser.Reader;
 import jasi.semantics.*;
+import jasi.semantics.procedure.PrimitiveProcedure;
 
 import java.util.logging.Logger;
 
@@ -10,8 +12,19 @@ public class Main {
     private final static Logger log = Logger.getLogger(Main.class.getName());
 
     public static void main(String[] args) {
+
+        Environment theGlobalEnv = createGlobalEnv();
+
         Scheme s = new Scheme();
+        
+        log.severe(":" + (s.eval(Reader.read(), theGlobalEnv)).toString() + ":");
+    }
+
+    private static Environment createGlobalEnv() {
         Environment env = new Environment();
-        log.severe(":" + (s.eval(Reader.read(), env)).toString() + ":");
+        
+        env.putBinding(SVariable.getInstance("+"), new PrimitiveProcedure(PrimitiveProcedure.PLUS));
+        env.putBinding(SVariable.getInstance("read"), new PrimitiveProcedure(PrimitiveProcedure.READ));
+        return env;
     }
 }
